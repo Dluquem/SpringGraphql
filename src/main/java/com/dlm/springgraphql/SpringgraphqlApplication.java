9ac1887ee21fdd12d965c68e394e1f93e7c31a80
Duilio Luque <dluque@pucp.pe>
2018-09-13 21:25:57 -0500

    primer commit

---
+++ src/main/java/com/dlm/springgraphql/SpringgraphqlApplication.java
@@ -0,0 +1,12 @@
+package com.dlm.springgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringgraphqlApplication.class, args);
+    }
+}
